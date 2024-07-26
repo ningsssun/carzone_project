95939ed62c90dd9c5807161ce48d8a921a733c46
sunning <ning.sssun@gmail.com>
2024-07-26 15:25:10 +0400

    car admin feature

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Car
 
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+
+    thumbnail.short_description = 'Car Image'
+
+    list_display = ('id', 'thumbnail', 'car_title', 'city', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id', 'thumbnail', 'car_title')
+    list_editable = ('is_featured', )
+
+    search_fields = ('id', 'car_title', 'city', 'model', 'body_style')
+
 # Register your models here.
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
