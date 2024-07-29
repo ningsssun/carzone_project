562585cfb4e9b5eb2ceae8439f1c38984c6844c2
sunning <ning.sssun@gmail.com>
2024-07-29 23:36:03 +0400

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,5 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('carzone.urls')),
-    path('cars/', include('cars.urls')),
+    path('cars', include('cars.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
