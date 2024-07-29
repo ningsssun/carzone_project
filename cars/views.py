39c756b2918db5213638b623a338813b94495139
sunning <ning.sssun@gmail.com>
2024-07-29 22:56:55 +0400

    admin feature

--- cars/views.py
+++ cars/views.py
@@ -9,8 +9,17 @@ def cars(request):
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
 
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     data = {
         'cars': paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
 
     return render(request, 'cars/cars.html', data)
@@ -27,6 +36,12 @@ def car_detail(request, id):
 def search(request):
     cars = Car.objects.order_by('-created_date')
 
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
@@ -35,31 +50,36 @@ def search(request):
     if 'model' in request.GET:
         model = request.GET['model']
         if model:
-            cars = cars.filter(description__iexact=model)
+            cars = cars.filter(model__iexact=model)
 
     if 'city' in request.GET:
         city = request.GET['city']
         if city:
-            cars = cars.filter(description__iexact=city)
+            cars = cars.filter(city__iexact=city)
 
     if 'year' in request.GET:
         year = request.GET['year']
         if year:
-            cars = cars.filter(description__iexact=year)
+            cars = cars.filter(year__iexact=year)
 
     if 'body_style' in request.GET:
         body_style = request.GET['body_style']
         if body_style:
-            cars = cars.filter(description__iexact=body_style)
+            cars = cars.filter(body_style__iexact=body_style)
 
     if 'min_price' in request.GET:
         min_price = request.GET['min_price']
         max_price = request.GET['max_price']
         if max_price:
-            cars.filter(price__gte=min_price, price__lte=max_price)
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
 
     data = {
         'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
     }
 
     return render(request, 'cars/search.html', data)
