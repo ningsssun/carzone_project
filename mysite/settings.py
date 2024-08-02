fc3c0e48cac0fc1c0d25819595e36080aab1740f
Ning Sun <139591917+ningsssun@users.noreply.github.com>
2024-08-02 18:11:16 +0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,7 +167,7 @@ SITE_ID = 1
 # Sending email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'monica05200320@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
